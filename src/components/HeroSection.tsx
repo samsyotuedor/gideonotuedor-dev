705831989b0360743daae7e8d01ab9ab947ed5c6
samsyotuedor <samsyotuedor40@gmail.com>
2026-01-16 09:16:07 +0100

    change the width size

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -300,7 +300,6 @@ export function HeroSection() {
             </section>
           </div>
 
-          {/* Scroll Indicator */}
           <motion.div
             className="absolute bottom-8 left-1/2 -translate-x-1/2"
             initial={{ opacity: 0 }}
