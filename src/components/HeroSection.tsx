a70b13efacc07bb53cfd66afa344bd7a013fedb0
samsyotuedor <samsyotuedor40@gmail.com>
2026-04-17 22:48:14 +0100

    install a package

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -282,21 +282,6 @@ export function HeroSection() {
             </motion.div>
             <section className="relative w-full md:1/2  h-screen">
               <ComputersCanvas />
-              {/* <div className="absolute xs:bottom-2 bottom-32 w-full flex justify-center items-center">
-                <a href="#about">
-                  <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-                    <motion.div
-                      animate={{ y: [0, 24, 0] }}
-                      transition={{
-                        duration: 1.5,
-                        repeat: Number.POSITIVE_INFINITY,
-                        repeatType: "loop",
-                      }}
-                      className="w-3 h-3 rounded-full bg-secondary mb-1"
-                    />
-                  </div>
-                </a>
-              </div> */}
             </section>
           </div>
 
